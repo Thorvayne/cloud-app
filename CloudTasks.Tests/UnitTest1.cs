9b41b82ec285db7236502d8c2703aa2a9eba2d78
Karol99976 <wrx92907@student.wroclaw.merito.pl>
2026-04-18 12:40:15 +0200

    Add xUnit test for CloudTask safety check

---
+++ CloudTasks.Tests/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using CloudBackend.Models;
+
+namespace CloudTasks.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void NewTask_ShouldNotBeCompleted()
+    {
+        // Arrange
+        var task = new CloudTask
+        {
+            Name = "Przetestować bezpiecznik"
+        };
+
+        // Assert
+        Assert.False(task.IsCompleted);
+    }
+}
